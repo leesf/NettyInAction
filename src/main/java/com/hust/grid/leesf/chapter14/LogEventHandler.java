ffbb68a5172b9b6c69d43bd42a4c855199ac348e
leesf <490081539@qq.com>
2017-05-30 11:50:39 +0800

    udp

---
+++ src/main/java/com/hust/grid/leesf/chapter14/LogEventHandler.java
@@ -0,0 +1,27 @@
+package com.hust.grid.leesf.chapter14;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class LogEventHandler extends SimpleChannelInboundHandler<LogEvent> {
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext channelHandlerContext, LogEvent event) throws Exception {
+        StringBuilder builder = new StringBuilder();
+        builder.append(event.getReceivedTimestamp());
+        builder.append(" [");
+        builder.append(event.getSource().toString());
+        builder.append("] [");
+        builder.append(event.getLogfile());
+        builder.append("] : ");
+        builder.append(event.getMsg());
+
+        System.out.println(builder.toString());
+    }
+}
